6e8305d5d37cd07e725d3030286aa95b929e0bbc
vfr750y <th91843@proton.me>
2025-10-01 11:57:57 +1300

    add constructor

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract RebaseToken {}
+contract RebaseToken is ERC20 {
+    constructor() ERC20("Rebast Token", "RBT") {}
+}
