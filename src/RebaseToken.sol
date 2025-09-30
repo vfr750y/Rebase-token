a53ae7bdd48382dcf5ae40be55468dfe8d04ad59
vfr750y <th91843@proton.me>
2025-10-01 12:01:19 +1300

    add natspec

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title Rebase Token
+ *
+ * @author Ajay Curry
+ * @notice This is a cross-chain rebase token that incentivises users to deposit inta a vault and gain interest in reqards
+ * @notice the interest rate in the smart contract can only decrease
+ * @notice Each user will have their own interest rate which is the global interest rate at the time.
+ */
 contract RebaseToken is ERC20 {
     constructor() ERC20("Rebast Token", "RBT") {}
 }
