cc4e0cbdf6087dc77e7852b500c6592b1981ce4b
vfr750y <th91843@proton.me>
2025-10-08 09:19:25 +1300

    add transferFrom function

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -116,6 +116,25 @@ contract RebaseToken is ERC20 {
     }
 
     /**
+     * @notice Trnasfer tokens from one user to another
+     * @param _sender The user to transfer the tokens from
+     * @param _recipient  The user to transfer the tokens to
+     * @param _amount  THe amount of tokens to transfer
+     */
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+        }
+        return super.transferFrom(_sender, _recipient, _amount);
+    }
+
+    /**
      * @notice Calculate the interst that has accumulated since the last update
      * @param _user The user to calculate the interest accumulated for
      * @return linearInterest The interest that has acumulated since the last update
