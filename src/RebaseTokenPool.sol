7c62cc4001088675bd0a54c0fe5fc854164df332
vfr750y <th91843@proton.me>
2025-10-16 11:25:10 +1300

    add RebaseTokenPool lockOrBurn and ReleaseOrMint functions for cross chain

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -5,6 +5,7 @@ import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 import {Pool} from "@ccip/contracts/src/v0.8/ccip/libraries/Pool.sol";
 import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IRebaseToken} from "./Interfaces/IRebaseToken.sol";
 
 contract RebaseTokenPool is TokenPool {
     constructor(IERC20 _token, address[] memory _allowlist, address _rmnProxy, address _router)
@@ -14,10 +15,24 @@ contract RebaseTokenPool is TokenPool {
     function lockOrBurn(Pool.LockOrBurnInV1 calldata lockOrBurnIn)
         external
         returns (Pool.LockOrBurnOutV1 memory lockOrBurnOut)
-    {}
+    {
+        _validateLockOrBurn(lockOrBurnIn);
+        address receiver = abi.decode(lockOrBurnIn.receiver, (address));
+        uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(receiver);
+        IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
+        lockOrBurnOut = Pool.LockOrBurnOutV1({
+            destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector),
+            destPoolData: abi.encode(userInterestRate)
+        });
+    }
 
     function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn)
         external
         returns (Pool.ReleaseOrMintOutV1 memory)
-    {}
+    {
+        _validateReleaseOrMint(releaseOrMintIn);
+        uint256 userInterestRate = abi.decode(releaseOrMintIn.sourcePoolData, (uint256));
+        IRebaseToken(address(i_token)).mint(releaseOrMintIn.receiver, releaseOrMintIn.amount, userInterestRate);
+        return Pool.ReleaseOrMintOutV1({destinationAmount: releaseOrMintIn.amount});
+    }
 }
