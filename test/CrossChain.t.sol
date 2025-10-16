5ebc5a00600bfcb05464ae2ccc53de8421f166e7
vfr750y <th91843@proton.me>
2025-10-17 08:14:23 +1300

    add CrossChainTest

---
+++ test/CrossChain.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^ 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {Vault} from "../src/Vault.sol";
+import {IRebaseToken} from "../src/Interfaces/IRebaseToken.sol";
+
+contract CrossChainTest is Test {
+    uint256 sepoliaFork;
+    uint256 arbSepoliaFork;
+
+    function setUp() public {
+        sepoliaFork = vm.createSelectFork("sepolia-eth");
+        arbSepoliaFork = vm.createFork("arb-sepolia");
+    }
+}
