81f5989f55aa908203cda41f2faed585058d9e6e
vfr750y <th91843@proton.me>
2025-10-17 09:02:18 +1300

    add deploy on arbsepolia

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -43,7 +43,8 @@ contract CrossChainTest is Test {
             sepoliaNetworkDetails.rmnProxyAddress,
             sepoliaNetworkDetails.routerAddress
         );
-
+        sepoliaToken.grantMintAndBurnRole(address(vault));
+        sepoliaToken.grantMintAndBurnRole(address(sepoliaPool));
         vm.stopPrank();
 
         // Deploy and confirguree on Arbitrum Sepolia
@@ -56,6 +57,7 @@ contract CrossChainTest is Test {
             arbSepoliaNetworkDetails.rmnProxyAddress,
             arbSepoliaNetworkDetails.routerAddress
         );
+        arbSepoliaToken.grantMintAndBurnRole(address(arbSepoliaPool));
         vm.startPrank(owner);
         vm.stopPrank();
     }
